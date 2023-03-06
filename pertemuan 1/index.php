43acb20b36d2d98f295cb1267f34a238b469fbf0
ahmadsaipullah <ahmadsaipullah911@gmail.com>
2023-03-06 22:33:44 +0700

    menambahkan file pertemuan 1

---
+++ pertemuan 1/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Junior Web Developer</title>
+</head>
+
+<body>
+
+  <h1>Data Diri</h1>
+
+  <?php echo "Nama : Ahmad Saifullah <br>
+              Universitas : Universitas Muhammadiyah Tangerang  <br>
+              Jurusan : Teknik Informatika <br>
+              Status : Kuliah
+  
+  "; ?>
+
+</body>
+
+</html>
